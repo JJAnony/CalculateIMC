f10812882c5456ea468c6a48404edfdd88d8b725
anonymous <jcolivati@gmail.com>
2021-03-04 00:14:14 -0300

    Final Calculator

--- app/src/main/java/br/com/jjolivati/calculateimc/MainActivity.kt
+++ app/src/main/java/br/com/jjolivati/calculateimc/MainActivity.kt
@@ -2,35 +2,33 @@ package br.com.jjolivati.calculateimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
     }
 
-    override fun onStart() {
-        super.onStart()
+    private fun setListeners() {
+        calculateButton.setOnClickListener {
+            calculate(textInputWeight.text.toString(), textInputHeight.text.toString())
+        }
     }
 
-    override fun onResume() {
-        super.onResume()
-    }
+    private fun calculate(weight: String, height: String) {
+        val weight = weight.toFloatOrNull()
+        val height = height.toFloatOrNull()
 
-    override fun onPause() {
-        super.onPause()
-    }
+        if (weight != null && height != null) {
+            val imc = weight / (height * height)
+            result.text = "Seu Imc é %.2f".format(imc)
+        }
 
-    override fun onStop() {
-        super.onStop()
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
     }
 
 }
